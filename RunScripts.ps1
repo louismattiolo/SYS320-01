e94c2e96046d696b5e4766d3278e5cef3b79aa02
Louis Mattiolo <130078590+louismattiolo@users.noreply.github.com>
2025-09-23 17:29:26 -0400

    Create RunScripts.ps1

---
+++ RunScripts.ps1
@@ -0,0 +1,8 @@
+. "C:\Users\champuser\Desktop\logonscript2.ps1"
+
+clear
+
+$Days = Read-Host "Days Make sure to add a "-" to your answer"
+
+Get-SystemStartShutdown($Days)
+Get-SystemStartShutdown($Days)
