84b53948b9e72e8d3c057d9e7225d93a1021a716
Louis Mattiolo <130078590+louismattiolo@users.noreply.github.com>
2025-09-23 15:47:55 -0400

    Create loginon.ps1
    Loginon script

---
+++ loginon.ps1
@@ -0,0 +1,26 @@
+
+
+$loginouts = Get-EventLog System -Source Microsoft-Windows-Winlogon -After (Get-Date).AddDays(-14)
+
+$loginoutsTable = @() #This is the empty array
+for ($i=0; $i -lt $loginouts.Count; $i++) {
+
+# This creates the event property value 
+$event = ""
+if($loginouts[$i].InstanceID -eq 7001) {$event="LogOn"}
+if($loginouts[$i].InstanceID -eq 7002) {$event="LogOff"}
+
+# This creates the user property value 
+$user = $loginouts[$i].ReplacementStrings[1] 
+
+
+# Adding each new line (in form of a custom object) to our empty array
+$loginoutsTable += [PSCustomObject]@{ "Time" = $loginouts[$i].TimeGenerated;
+                                        "Id"   = $loginouts[$i].InstanceId;
+                                        "Event" = $event;
+                                        "User" = $user;
+    }
+}
+
+# End of for 
+$loginoutsTable
