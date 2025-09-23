ab2d056b4bb8c9888b65ae17691951073ae8c76f
Louis Mattiolo <130078590+louismattiolo@users.noreply.github.com>
2025-09-23 16:27:43 -0400

    Update loginon.ps1

--- loginon.ps1
+++ loginon.ps1
@@ -1,4 +1,6 @@
-$loginouts = Get-EventLog System -Source Microsoft-Windows-Winlogon -After (Get-Date).AddDays(-14)
+function LoginRecords($Days){
+
+$loginouts = Get-EventLog System -Source Microsoft-Windows-Winlogon -After (Get-Date).AddDays($Days)
 
 $loginoutsTable = @() #This is the empty array
 for ($i=0; $i -lt $loginouts.Count; $i++) {
@@ -9,16 +11,19 @@ if($loginouts[$i].InstanceID -eq 7001) {$event="LogOn"}
 if($loginouts[$i].InstanceID -eq 7002) {$event="LogOff"}
 
 # This creates the user property value 
-$user = $loginouts[$i].ReplacementStrings[1] 
+# $user = $loginouts[$i].ReplacementStrings[1] 
 
+$user = (New-Object System.Security.Principal.SecurityIdentifier $loginouts[$i].ReplacementStrings[1]).Translate([System.Security.Principal.NTAccount])
 
 # Adding each new line (in form of a custom object) to our empty array
 $loginoutsTable += [PSCustomObject]@{ "Time" = $loginouts[$i].TimeGenerated;
                                         "Id"   = $loginouts[$i].InstanceId;
                                         "Event" = $event;
                                         "User" = $user;
+                                        
+    
     }
 }
 
-# End of for 
-$loginoutsTable
+return $loginoutsTable
+}
