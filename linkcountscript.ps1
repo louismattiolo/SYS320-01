5fbf8f83033644122069938bf84cb65bf40e1c1e
Louis Mattiolo <130078590+louismattiolo@users.noreply.github.com>
2025-10-04 15:31:49 -0400

    uplaod
    scraping n linkcounting
    
    i can count to 10 1 2 3 4 5 6 7 8 9 10
    aren't you impressed??

---
+++ linkcountscript.ps1
@@ -0,0 +1,19 @@
+﻿$scraped_page = Invoke-WebRequest -Uri "http://10.0.17.23/ToBeScraped.html" 
+
+#Get a count of the links in the page
+$scraped_page.Links.Count
+
+#Displays links as html elements 
+$scraped_page.links
+
+#Display only URL and its text
+$scraped_page.Links | Select-Object -ExpandProperty OuterHtml
+
+#Get outer text of every element the h2 tag
+
+$h2s = $scraped_page.ParsedHtml.body.getElementsByTagName("h2") | Select-Object outerText
+$h2s
+
+# Print innerText of every div element that has the class as "div-1"
+$divs1 = $scraped_page.ParsedHtml.body.getElementsByTagName("div") | Where-Object { $_.className -like "*div-1*" } | Select-Object -ExpandProperty innerText
+$divs1
---
+++ scrapingclassps1.ps1
@@ -0,0 +1,31 @@
+﻿function gatherClasses() {
+    $page = Invoke-WebRequest -TimeoutSec 2 http://10.0.17.23/Courses2025FA.html
+
+    # get  <tr> (table row) 
+    $trs = $page.ParsedHtml.body.getElementsByTagName("tr")
+
+    # empty array   
+    $FullTable = @()
+    for($i=1; $i -lt $trs.length; $i++){
+    
+        # Get all <td> (table data/cell) elements within the current row
+        $tds = $trs[$i].getElementsByTagName("td")
+      
+        # separate the start time and end time from the 1 time field
+        $Times = $tds[5].innerText.Split('-')
+
+        $FullTable += [PSCustomObject]@{
+            "Class Code" = $tds[0].innerText; 
+            "Title"      = $tds[1].innerText; 
+            "Days"       = $tds[4].innerText; 
+            "Time Start" = $Times[0]; 
+            "Time End"   = $Times[1]; 
+            "Instructor" = $tds[6].innerText; 
+            "Location"   = $tds[9].innerText; 
+            
+        }
+    } 
+    
+    return $FullTable
+}
+return $FullTable
